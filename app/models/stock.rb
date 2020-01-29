b16fa2d3e900a2b3018f07aff0772c10642c6fda
Philippe Gilles <philgdep@gmail.com>
2020-01-29 12:12:37 +0100

    setup credentials and getting stick prices

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex[:sandbox_token], endpoint: "https://sandbox.iexapis.com/v1")
+    client.price(ticker_symbol)
+  end
 end
