903087119b95c2e196cf432dfddb95ecb3e128c0
Philippe Gilles <philgdep@gmail.com>
2020-01-31 13:40:42 +0100

    add search friend

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create,:destroy]
   devise_for :users
   root "pages#index"
-
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search_friend', to: 'users#search'
 end
