d95a66ae6582934e3f748f374b4c597dcb671d11
Philippe Gilles <philgdep@gmail.com>
2020-02-01 10:42:26 +0100

    filter current user from search options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @tracked_stocks = current_user.stocks
+    @tracked_friends = current_user.friends
   end
 
   def my_friends
@@ -8,6 +8,24 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends.length > 0
+        respond_to do |format|
+          format.js { render partial: "users/friend-result" }
+        end
+      else
+        respond_to do |format|
+          flash.now[:notice] = "Nobody found !"
+          format.js { render partial: "users/friend-result" }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter something to search"
+        format.js { render partial: "users/friend-result" }
+      end
+    end
   end
 end
